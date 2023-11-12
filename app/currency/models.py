813e2b1b88102640399b5bca7f52a25d877b5fb1
Serhii <SerezaTuma@gmail.com>
2023-11-12 16:32:54 +0200

    Rate list and ContactUs list(homework5)

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Rate(models.Model):
+    buy = models.DecimalField(max_digits=6, decimal_places=2)
+    sell = models.DecimalField(max_digits=6, decimal_places=2)
+    type = models.CharField(max_length=4)
+    source = models.CharField(max_length=255)
+    created = models.DateTimeField()
+
+
+class ContactUs(models.Model):
+    email_from = models.EmailField(max_length=254)
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from currency.views import rate_list, contactus_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rate/list/', rate_list),
+    path('contactus/list/', contactus_list),
+
 ]
